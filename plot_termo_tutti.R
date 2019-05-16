feb059a754ad24f15a171bdcaf176adf0b2316b8
mariaranci <31917865+mariaranci@users.noreply.github.com>
2019-05-16 10:59:17 +0200

    aggiunto nome dei termometri
    plotto l'andamento dei termometri elencati nel file di richiesta dell'estrattore.
    Sotto la query per individuare i termometri assegnati a XX attivi dal ... al ...
    
    select distinct(staz.IDstazione), IDsensore,Provincia,CONCAT(Comune," ",IFNULL(Attributo,"")), IDsensore ,Acronimo from A_Stazioni as staz, A_Sensori as sens , Utenti as ut, StazioniAssegnate as ass where ass.IDstazione=staz.IDstazione and ass.IDutente=ut.IDutente and staz.IDstazione=sens.IDstazione and NOMEtipologia="T" and sens.DataInizio<"YYYY-MM-GG" and (sens.DataFine is null or sens.DataFine>"YYYY-MM-GG") and IDrete in (1,4) and Acronimo="XX" order by Provincia;

--- plot_termo_tutti.R
+++ plot_termo_tutti.R
@@ -1,32 +1,109 @@
-nomefile<-list.files(path=".", pattern="*_R_[1,2,3]*.txt")
-nomefile<-sort(nomefile)
-print(nomefile)
-colore<-rep(palette(),1,length(nomefile))
-IDsensore<-NA
-IDfunzione<-NA
-for (i in 1:length(nomefile)) {IDsensore[i]<-as.character(strsplit(nomefile[i], "_")[[1]][1])}
-for (i in 1:length(nomefile)) {IDfunzione[i]<-strsplit(as.character( strsplit(nomefile[i], "_")[[1]][3]),".",fixed=T)[[1]][1]}
-for (i in 1:length(nomefile)){
-  lettura<-try(read.csv(nomefile[i],header=FALSE,na.strings = "-999",colClasses=c("integer","character","numeric","numeric"),sep="\t"),silent=TRUE)
-  if (inherits(lettura,"try-error")) {
-    cat("....... ERRORE !! file: ", nomefile[i],"  ...........................\n",file="prova.log",append=T)
-    i<-i+1
-    break
+#####################################
+dir<-"./"
+inizio<-as.POSIXct(strptime("2018/01/01", format="%Y/%m/%d"))
+fine<-as.POSIXct(strptime("2019/05/01", format="%Y/%m/%d"))
+Tmin<--30
+Tmax<-45
+file_ric <- "Richiesta.txt"
+file_out <- "Estremi.csv"
+# 0 - creazione pdf, 1 - grafico a video interattivo 
+interattivo <- 0
+####################################
+
+
+#preparazione output
+cat("TERMOMETRI \n",file=file_out)
+cat("---","Nome", "IDsensore","min","oramin","max","oramax","\n",sep="\t",file=file_out,append=T)
+file_log<-paste(dir,"plot.log",sep="")
+cat("Termometri ", date(),"\n",file=file_log)
+
+# lettura file di richiesta per individuare i sensori
+richiesta<- read.csv ( file_ric , header = FALSE, as.is = TRUE, sep="\t")
+IDsensore<-richiesta$V2
+
+# lettura dell'anagrafica REM per dare un nome ai sensori
+anagrafica <- read.csv ( "file:///Y:/AnagraficaSensori.csv" , header = TRUE ,  quote="\"", as.is = TRUE, sep=";", na.strings = "-9999")
+
+# colori per min-avg-max)
+colore=c("green","black","red")
+
+# ciclo su sensori elencati nel file di richiesta dell'estrattore e eseguo pdf del grafico e file csv 
+for (i in 1:length(IDsensore)){
+  
+  oo<-which(anagrafica$IdSensore==IDsensore[i])
+  if(is.na(anagrafica$Attributo[oo])==TRUE) {
+    Nome <- anagrafica$Comune[oo]
+  }else{
+    Nome <- paste(anagrafica$Comune[oo], anagrafica$Attributo[oo] ,sep="_")
   }
-  print(lettura[1,])
-  print(i)
-  marche_temporali<-as.POSIXct(strptime(lettura$V2, format="%Y-%m-%d %H:%M:%S"))
-  cat("\n",nomefile[i],":   Dal ",min(lettura$V2)," al ", max(lettura$V2) ,"\n",sep="",file="prova.log",append=T)
-  cat("\n","minimo:   ", min(lettura$V3,na.rm=TRUE),"  data minimo:  ", lettura$V2[which.min(lettura$V3)],sep="",file="prova.log",append=T)
-  cat("\n","massimo:  ", max(lettura$V3,na.rm=TRUE),"  data massimo:  ", lettura$V2[which.max(lettura$V3)],sep="","\n",file="prova.log",append=T)
-  cat(IDsensore[i],IDfunzione[i],min(lettura$V3,na.rm=TRUE),lettura$V2[which.min(lettura$V3)],max(lettura$V3,na.rm=TRUE),lettura$V2[which.max(lettura$V3)],"\n",sep=";",file="estremi.txt",append=T)
-   if (i==1){
-    plot(marche_temporali,(lettura$V3),main=paste("Termometro",IDsensore[i],sep=""),type="l",col=colore[i])}
-  else if(IDsensore[i]!=IDsensore[i-1]){
-    plot(marche_temporali,(lettura$V3),main=paste("Termometro",IDsensore[i],sep=""),type="l",col=colore[i])}
-  else {
-    lines(marche_temporali,(lettura$V3),col=colore[i+1])}
-  i<-i+1
-  par(ask = TRUE)
+  
+  cat("\n",file=file_out,append=T)
+  
+#########################################################################################
+  
+## modalità interattiva
+  if (interattivo==1){
+   par(ask = TRUE)
+  }else if(interattivo==0){
+   ## direttamente su file
+   nomepdf <- paste(IDsensore[i],"_",Nome,".pdf",sep="")
+   pdf(nomepdf)
+  }else{
+   print("ATTENZIONE: interattivo deve essere 0 o 1")
+   break
+  }
+  
+#################################################################
+  #    
+  minimo <- paste(path=dir, IDsensore[i], "_R_2.txt",sep="")
+  medie  <- paste(path=dir, IDsensore[i], "_R_1.txt",sep="")
+  max    <- paste(path=dir, IDsensore[i], "_R_3.txt",sep="")
+  #min_orario<-paste(path=dir, IDsensore[i], "_G_13.txt",sep="")
+  #max_orario<-paste(path=dir, IDsensore[i], "_G_12.txt",sep="")
+  
+  if(file.exists(minimo)){
+    lettura_min<-try(read.csv(minimo,header=FALSE,na.strings = "-999",colClasses=c("integer","character","numeric","numeric"),sep="\t"),silent=TRUE)
+    if (inherits(lettura_min,"try-error")) {
+      cat("....... ERRORE !! file:   ...........................\n",file=file_log,append=T)
+    }
+    data_del_min <- as.POSIXct(strptime(lettura_min$V2[which.min(lettura_min$V3)], format="%Y-%m-%d %H:%M:%S"))
+    data_del_max <- as.POSIXct(strptime(lettura_min$V2[which.max(lettura_min$V3)], format="%Y-%m-%d %H:%M:%S"))
+    cat("min",Nome,IDsensore[i],min(lettura_min$V3,na.rm=TRUE),format(data_del_min,format="%Y-%m-%d %H:%M"),max(lettura_min$V3,na.rm=TRUE),format(data_del_max,format="%Y-%m-%d %H:%M"),"\n",sep="\t",file=file_out,append=T)
+    marche_temporali_min<-as.POSIXct(strptime(lettura_min$V2, format="%Y-%m-%d %H:%M:%S"))
+    plot(marche_temporali_min,lettura_min$V3,xlim=c(inizio,fine),ylim<-c(Tmin,Tmax),main=paste(Nome , IDsensore[i],sep=" - "),type="l",col=colore[1])
+   }
+  
+  lettura_medie<-try(read.csv(medie,header=FALSE,na.strings = "-999",colClasses=c("integer","character","numeric","numeric"),sep="\t"),silent=TRUE)
+   if (inherits(lettura_medie,"try-error")) {
+    cat("....... ERRORE !! file:  ...........................\n",file=file_log,append=T)
+   }
+  data_del_min <- as.POSIXct(strptime(lettura_medie$V2[which.min(lettura_medie$V3)], format="%Y-%m-%d %H:%M:%S"))
+  data_del_max <- as.POSIXct(strptime(lettura_medie$V2[which.max(lettura_medie$V3)], format="%Y-%m-%d %H:%M:%S"))
+  cat("Avg",Nome,IDsensore[i],min(lettura_medie$V3,na.rm=TRUE),format(data_del_min,format="%Y-%m-%d %H:%M"),max(lettura_medie$V3,na.rm=TRUE),format(data_del_max,format="%Y-%m-%d %H:%M"),"\n",sep="\t",file=file_out,append=T)
+  marche_temporali_medie<-as.POSIXct(strptime(lettura_medie$V2, format="%Y-%m-%d %H:%M:%S"))
+  #
+   if(file.exists(minimo)){
+    lines(marche_temporali_medie,lettura_medie$V3,col=colore[2])
+   }else{
+    plot(marche_temporali_medie,lettura_medie$V3,xlim=c(inizio,fine),ylim<-c(Tmin,Tmax),main=paste(Nome , IDsensore[i],sep=" - "),type="l",col=colore[2])
+   }
+ 
+   if(file.exists(max)){
+    lettura_max<-try(read.csv(max,header=FALSE,na.strings = "-999",colClasses=c("integer","character","numeric","numeric"),sep="\t"),silent=TRUE)
+    if (inherits(lettura_max,"try-error")) {
+      cat("....... ERRORE !! file:  ...........................\n",file=file_log,append=T)
+    }
+   
+    data_del_min <- as.POSIXct(strptime(lettura_max$V2[which.min(lettura_max$V3)], format="%Y-%m-%d %H:%M:%S"))
+    data_del_max <- as.POSIXct(strptime(lettura_max$V2[which.max(lettura_max$V3)], format="%Y-%m-%d %H:%M:%S"))
+    cat("Max",Nome,IDsensore[i],min(lettura_max$V3,na.rm=TRUE),format(data_del_min,format="%Y-%m-%d %H:%M"),max(lettura_max$V3,na.rm=TRUE),format(data_del_max,format="%Y-%m-%d %H:%M"),"\n",sep="\t",file=file_out,append=T)
+    marche_temporali_max<-as.POSIXct(strptime(lettura_max$V2, format="%Y-%m-%d %H:%M:%S"))
+    lines(marche_temporali_max,lettura_max$V3,col=colore[3])
+   }
+  
+  legend(inizio,Tmax,c("min","med","max"), col=c(colore[1],colore[2],colore[3]),lty=1,cex=1,lwd=2)
+  
+   i<-i+1
+   print(Nome)
   }
 
